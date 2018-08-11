09ba49a4085a24349e4526516b70fecc7025ea74
Norvin Yecla <ngydev@outlook.com>
2018-08-11 21:53:35 +1000

    Import db from app folder

--- mycroblog/models.py
+++ mycroblog/models.py
@@ -1,8 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
+from mycroblog import db
 
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
