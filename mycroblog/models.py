aab18dff4e9be3c08b4617ee242080b8f3985bcb
Norvin Yecla <ngydev@outlook.com>
2018-08-13 21:51:03 +1000

    Add migration file for entries table

--- mycroblog/models.py
+++ mycroblog/models.py
@@ -1,8 +1,22 @@
 from mycroblog import db
 
 class Entry(db.Model):
+    __tablename__ = 'entries'
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(100))
+    text = db.Column(db.String(140))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
 
     def __repr__(self):
         return '%s' % self.text
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.remove(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Entry.query.all()
